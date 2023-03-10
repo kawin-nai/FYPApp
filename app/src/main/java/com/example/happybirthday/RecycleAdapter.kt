a3608a1203daee23b5fd7835d7187f0d17eafda4
qazseee <qazseee@gmail.com>
2023-03-10 19:50:25 +0800

    Added person name to recycler view

--- app/src/main/java/com/example/happybirthday/RecycleAdapter.kt
+++ app/src/main/java/com/example/happybirthday/RecycleAdapter.kt
@@ -1,4 +1,38 @@
 package com.example.happybirthday
 
-class RecycleAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecycleAdapter(private val context: Context,
+                     private var names: List<String>,
+                     private var roles: List<String>,
+                     private var images: List<Int>,
+                     ) : RecyclerView.Adapter<RecycleAdapter.ViewHolder>() {
+
+    class ViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+        val nameView: TextView = view.findViewById(R.id.person_name)
+        val roleView: TextView = view.findViewById(R.id.person_role)
+        val imgView: ImageView = view.findViewById(R.id.iv_image)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.person_item, parent, false)
+        return ViewHolder(adapterLayout)
+    }
+
+    override fun getItemCount(): Int {
+        return names.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.nameView.text = names[position]
+        holder.roleView.text = roles[position]
+        holder.imgView.setImageResource(images[position])
+    }
 }
--- app/src/main/java/com/example/happybirthday/SuccessActivity.kt
+++ app/src/main/java/com/example/happybirthday/SuccessActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.happybirthday.databinding.ActivitySuccessBinding
 import com.google.gson.Gson
 
 class SuccessActivity : AppCompatActivity() {
 
     private val gson = Gson()
+    private var namesList = mutableListOf<String>()
+    private var rolesList = mutableListOf<String>()
+    private var imagesList = mutableListOf<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivitySuccessBinding.inflate(layoutInflater)
@@ -20,14 +24,33 @@ class SuccessActivity : AppCompatActivity() {
             val intent = Intent(this, CameraActivity::class.java)
             startActivity(intent)
         }
+
+        binding.rvRecyclerView.adapter = RecycleAdapter(this, namesList, rolesList, imagesList)
+        binding.rvRecyclerView.setHasFixedSize(true)
+
         val apiResponseBody = intent.getStringExtra("apiResponseBody")
         val jsonResponse = gson.fromJson(apiResponseBody, FaceVerificationResponse::class.java)
+
+        postToList(jsonResponse.content)
+
         Log.d("JSON Response", jsonResponse.toString())
         binding.apiResponse.movementMethod = ScrollingMovementMethod()
         binding.apiResponse.text = apiResponseBody
         binding.uploadButton.setOnClickListener {
             val intent = Intent(this, UploadActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun addToList(name: String, role: String, image: Int) {
+        namesList.add(name)
+        rolesList.add(role)
+        imagesList.add(image)
+    }
+
+    private fun postToList(faceList: ArrayList<FaceDetail>) {
+        for (i in faceList) {
+            addToList(i.person_name, "Student", R.mipmap.ic_new_launcher_round)
         }
     }
 }
